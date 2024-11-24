650f8a6175e63de6312e1b7b8d51a1b1de37fa92
Cheulong Sear <searcheulong@gmail.com>
2024-11-24 16:49:56 +0700

    06 precss sass

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-import './src/assets/05.css';
+import './src/assets/06.scss';
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import Inspect from 'vite-plugin-inspect'
 import tailwindcss from '@tailwindcss/vite';
 
 export default {
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler' // or "modern"
+      }
+    }
+  },
   plugins: [
     Inspect(), tailwindcss(),
   ],
