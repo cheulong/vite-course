69178c4eaa292689c75888c25dd107568b66fbf9
Cheulong Sear <searcheulong@gmail.com>
2024-11-26 22:23:14 +0700

    10.glob import

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-import imageUrl from './src/assets/js_logo.svg';
+const modules = import.meta.glob("/src/10/*.js", { eager: true });
+// const modules = import.meta.glob("/src/10/*.js");
+console.log(modules);
 
-console.log(imageUrl);
+// Object.values(modules).forEach(async (module) => {
+//   const data = await module();
+//   console.log(data);
+// });
 
-document.getElementById('image').src = imageUrl;
---
+++ src/10/a.js
@@ -0,0 +1,5 @@
+console.log("a");
+const a = 1;
+
+export default 5;
+export { a };
